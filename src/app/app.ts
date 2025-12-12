c2a6a0aa98ed8b3d1c73dda0df7eca8a2ebec264
Nis5680 <nishitshah5680@gmail.com>
2025-12-12 16:28:05 +0530

    3rd_change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,32 +1,34 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { email } from '@angular/forms/signals';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  students = ['John', 'Jane', 'Jim'];
+  // students = ['John', 'Jane', 'Jim'];
 
-  studdentsData=[
-    {
-      name:'John',
-      age:20,
-      email:'john@gmail.com'
-    },
-    {
-      name:'Jane',
-      age:22,
-      email:'jane@gmail.com'
-    },
-    {
-      name:'Jim',
-      age:21,
-      email:'jim@gmail.com'
-    }
-  ]
+  // studdentsData=[
+  //   {
+  //     name:'John',
+  //     age:20,
+  //     email:'john@gmail.com'
+  //   },
+  //   {
+  //     name:'Jane',
+  //     age:22,
+  //     email:'jane@gmail.com'
+  //   },
+  //   {
+  //     name:'Jim',
+  //     age:21,
+  //     email:'jim@gmail.com'
+  //   }
+  // ]
+
+  login=true;
 }
